2d78ab9f9ce16a04d326d8decba6b4ba4141b4e9
dagmawibabi <1babidagi@gmail.com>
2023-09-11 04:10:35 +0300

    project init

---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import logo from "../images/logo/logo.png";
+
+export default function Navigation() {
+    return  (
+        <div className="flex justify-between border-b border-b-cyan-950 border-opacity-50 p-5 items-center">
+            <img src={logo} alt="logo" className="w-72"></img>
+            <div className="flex justify-between w-56 font-bold text-base hover:[&>span]:text-cyan-400 uppercase">
+                <span> Home </span>
+                <span> About </span>
+                <span> Contact </span>
+            </div>
+        </div>
+    )
+}
