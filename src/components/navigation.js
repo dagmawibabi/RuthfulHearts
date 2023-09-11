57f2f375d3fd74141af68ab7c610dce23073dca4
dagmawibabi <1babidagi@gmail.com>
2023-09-11 04:25:07 +0300

    meta change

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ export default function Navigation() {
     return  (
         <div className="flex justify-between border-b border-b-cyan-950 border-opacity-50 p-5 items-center">
             <img src={logo} alt="logo" className="w-72"></img>
-            <div className="flex justify-between w-56 font-bold text-base hover:[&>span]:text-cyan-400 uppercase">
+            <div className="flex justify-between w-56 font-bold text-base hover:[&>span]:text-cyan-400 uppercase cursor-pointer">
                 <span> Home </span>
                 <span> About </span>
                 <span> Contact </span>
