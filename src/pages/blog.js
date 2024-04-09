c15d3a45463ebd7b89ab352ab15581f5e6e189ac
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-04-09 13:00:40 +0300

    fix:ui issues , routing and more

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -57,34 +57,35 @@ const Blog = () => {
         loading?(
           <BlogFallback/>
         ):(
-          <section className='z-20 flex px-1 md:px-12 lg:px-36 flex-col gap-4  items-center w-full min-h-screen'>
-          <div className="post-header flex flex-col gap-2 w-[95%] border-b border-b-gray-800">
-                <div className="image-container relative w-full border-none">
-                    <img className={`object-cover h-[26rem] border-2 border-[#1d4b5830]`} src={post.mainImage && urlForImage(post.mainImage).url()} alt={post.title}/>
-                </div>
-                <div className=''>
-                    <div className='flex justify-between items-center w-full'>
-                        <Link to="/" className='text-sm text-gray-400 px-4'>return to home</Link>
-                        <div className="author-avatar flex gap-2 h-fit items-center  " >
-                            <img className='h-[2rem] w-[2rem] rounded-full border-2 border-stone-600 justify-end items-center bg-[#1d4b5830] ' src={post.author ? urlForImage(post.author.image).url() : "fallback-image-url" }  alt='avatar'/>
-                            <ul className="flex flex-col justify-center">
-                              <li className='text-[10px] text-gray-400'>written by</li>
-                              <li className='text-gray-200'>{post.author && post.author.name}</li>
-                            </ul>
+          <section className='z-20 flex px-6 md:px-12 lg:px-36 flex-col gap-4  items-center w-full min-h-screen'>
+              <div className="z-20 flex  flex-col gap-4  items-center w-[95%] md:w-[80%] min-h-screen">
+                  <div className="post-header flex flex-col gap-2 border-b border-b-gray-800">
+                        <div className="image-container relative w-full border-none">
+                            <img className={`w-full h-full border-2 border-[#1d4b5830]`} src={post.mainImage && urlForImage(post.mainImage).url()} alt={post.title}/>
                         </div>
-                    </div>
-                    <h1 className="title text-start p-0 mt-1  md:text-4xl">{post.title}</h1>
-                </div>
+                        <div className=''>
+                            <div className='flex justify-between items-center w-full'>
+                                <Link to="/" className='text-sm text-gray-400 px-4'>return to home</Link>
+                                <div className="author-avatar flex gap-2 h-fit items-center  " >
+                                    <img className='h-[2rem] w-[2rem] rounded-full border-2 border-stone-600 justify-end items-center bg-[#1d4b5830] ' src={post.author ? urlForImage(post.author.image).url() : "fallback-image-url" }  alt='avatar'/>
+                                    <ul className="flex flex-col justify-center">
+                                      <li className='text-[10px] text-gray-400'>written by</li>
+                                      <li className='text-gray-200'>{post.author && post.author.name}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <h1 className="title text-start p-0 mt-1  md:text-4xl">{post.title}</h1>
+                        </div>
 
-                <div className='flex w-full justify-start'>                    
-                    <ShareBar post={post}/>               
-                </div>
-          </div>
-
-          <div className="content px-6 flex flex-col gap-4 justify-center w-full">
-                    <PortableTextEditor  body={post.body}/>
-          </div>
-           
+                        <div className='flex w-full justify-start'>                    
+                            <ShareBar post={post}/>               
+                        </div>
+                  </div>
+                  <div className="content flex flex-col gap-4 justify-center w-full">
+                            <PortableTextEditor  body={post.body}/>
+                  </div>
+              
+              </div>
 
 
 
