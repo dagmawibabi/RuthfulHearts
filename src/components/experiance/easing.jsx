5b2e6028befb78b636ca57819578ec6b09cef1c1
freab <fremesfin1@gmail.com>
2024-04-21 15:14:56 +0300

    fix the scroll on phone

--- src/components/experiance/easing.jsx
+++ src/components/experiance/easing.jsx
@@ -1,35 +1,19 @@
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
-import { Image, Float } from "@react-three/drei";
 
 const Easing = () => {
   useFrame((state, delta) => {
-    state.events.update();
+    state.events.update(); // Raycasts every frame rather than on pointer-move
     easing.damp3(
       state.camera.position,
-      [-state.pointer.x * 2, state.pointer.y + 0.5, 5.4],
+      [-state.pointer.x * 2, state.pointer.y + 0.5, 5.5],
       0.3,
       delta
-    );
-    state.camera.lookAt(0, 0, 0);
+    ); // Move camera
+    state.camera.lookAt(0, 0, 0); // Look at center
   });
 
-  const images = [];
-  const imageCount = 30;
-  const maxXOffset = 3.5;
-  const maxYOffset = 1.5;
-  for (let i = 0; i < imageCount; i++) {
-    const xOffset = (Math.random() - 0.5) * maxXOffset * 2;
-    const yOffset = (Math.random() - 0.5) * maxYOffset * 2;
-    const zOffset = -1;
-    images.push(
-      <Float key={i} position={[xOffset, yOffset, zOffset]}>
-        <Image url="./H.png" scale={0.3} />
-      </Float>
-    );
-  }
-
-  return <>{images}</>;
+  return null; // Since this component doesn't render anything, return null
 };
 
 export default Easing;
